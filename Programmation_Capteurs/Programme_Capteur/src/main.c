1f3ce7c97d84ff61f8e413fa1e83b11690a68998
Gaël <gael.chauvet@gramacitee.com>
2025-12-12 16:42:16 +0100

    test du Ruban LED SK6812 non-concluant passage possible à la tech LTE-M pour avancer

--- Programmation_Capteurs/Programme_Capteur/src/main.c
+++ Programmation_Capteurs/Programme_Capteur/src/main.c
@@ -25,7 +25,7 @@
 #define SW2_NODE DT_ALIAS(sw3)
 
 //SPI ports: SCK:13 MISO:12 MOSI:11
-#define STRIP_NODE DT_NODELABEL(led_strip0)
+#define STRIP_NODE DT_NODELABEL(spi2)
 
 // ---
 
@@ -97,7 +97,6 @@ void trigger_pulled(const struct device *dev, struct gpio_callback *cb, uint32_t
 // workhandler that call the listening routine to get raw IR signals
 static void lR_IR_WorkHandler(struct k_work *work)
 {
-
     uint8_t rawSignal = gpio_pin_get_dt(&irecv);
     
     lR_IR_Receive(&irParams, rawSignal);
@@ -130,23 +129,72 @@ static void emitIR_WorkHandler(struct k_work *work)
 
 // --- 
 
-void test_RGB_Strip_LEDs(const struct device *dev, struct spi_cs_control *cs){
+void write_RGB_Strip_LEDs(const struct device *dev, struct spi_cs_control *cs, uint8_t* RGB_data, int num_leds){
     struct spi_config config;
 
-    config.frequency = 800000;
-    config.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(24) | SPI_TRANSFER_MSB;
+    //frequency is 4MHz, it is more than necessary, so we will send 11110 for 1 and 10000 for 0, to follow protocol as described in the SK6812 datasheet
+    //It turns byte into 4 times the size (uint32 per byte)
+    config.frequency = 4000000;
+    config.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB;
     config.slave = 0;
     config.cs = *cs;
 
-    uint32_t buff[3] = { 0xFF0000, 0x00FF00, 0x0000FF };
-    int len = 3*sizeof(buff[0]);
+    uint64_t converted_data;
+    uint64_t uint64_buff[3]; //each LED requires 3 uint32 (G,R,B)
 
-    struct spi_buf tx_buf = { .buf = buff, .len = len };
-    struct spi_buf_set tx_bufs = { .buffers = &tx_buf, .count = 1 };
-    int ret = spi_write(dev, &config, &tx_bufs);
-    if (ret != 0) {
-        LOG_ERR("SPI write failed: %d", ret);
+    for(int i = 0; i < num_leds; i++){
+        // Each LED requires uint32 bytes (G, R, B)
+        // Data is expected in GRB format
+        // Assuming RGB_data is an array of size 3
+        for(int j = 0; j < 3; j++){
+            converted_data = 0;
+            for(int k =0; k < 8; k++){
+                //MSB first
+                if(RGB_data[j] & (1 << (8-i)) ){
+                    //it's a "1"
+                    converted_data =(converted_data << 1) | 1; // append 1
+                    converted_data =(converted_data << 1) | 1; // append 1
+                    converted_data =(converted_data << 1) | 1; // append 1
+                    converted_data =(converted_data << 1) | 1; // append 1
+                    converted_data =(converted_data << 1) ; // append 0
+                }
+                else{
+                    //it's a "0"
+                    converted_data =(converted_data << 1) | 1; // append 1
+                    converted_data =(converted_data << 1) ; // append 0
+                    converted_data =(converted_data << 1) ; // append 0
+                    converted_data =(converted_data << 1) ; // append 0
+                    converted_data =(converted_data << 1) ; // append 0
+
+                }
+            }
+            uint64_buff[j] = converted_data;
+            //LOG_DBG("Converted data : %llu", converted_data);
+        }
+
+        uint8_t buff[15]; //5 octets par couleurs 
+
+        for(int m =0; m <3; m++){
+            //on redivise ce qu'on doit envoyer en octet pour le SPI (et pour éviter d'envoyer des données en trop)+
+            for(int n =0; n <5; n++){
+                buff[m*5 + n] = (uint64_buff[m] >> (32 - 8*(n+1) ) ) & 0xFF; //extract each byte from the uint32
+            }
+            
+        }
+
+        //uint8_t buff[3] = { 0xAC, 0xAD, 0xAE };
+        int len = 15*sizeof(buff[0]);
+
+        struct spi_buf tx_buf = { .buf = buff, .len = len };
+        struct spi_buf_set tx_bufs = { .buffers = &tx_buf, .count = 1 };
+        int ret = spi_write(dev, &config, &tx_bufs);
+        if (ret != 0) {
+            LOG_ERR("SPI write failed: %d", ret);
+        }
+
     }
+
+    
 }
 
 // ---
@@ -206,11 +254,12 @@ int main(void)
     const struct device *const dev = DEVICE_DT_GET(STRIP_NODE);
     struct spi_cs_control cs_ctrl = (struct spi_cs_control){.gpio = GPIO_DT_SPEC_GET(STRIP_NODE, cs_gpios), .delay = 0u,};
 
+    uint8_t RGB_to_send[] = {0xFF, 0x00, 0xFF}; // carefull the format is Green Red Blue
 
     while(1){
 
         
-        test_RGB_Strip_LEDs(dev, &cs_ctrl);
+        write_RGB_Strip_LEDs(dev, &cs_ctrl, RGB_to_send , 2); //cyan
 
 
         val = gpio_pin_get_dt(&irecv);
@@ -221,7 +270,7 @@ int main(void)
         int res = decodeNEC(&irParams, &decodedResult);
 
         if(res == 0){
-            LOG_DBG("Decoded Result: 0x%08lX, bits: %d, res: %d \n", decodedResult.value, decodedResult.bits, res);  
+            LOG_DBG("Decoded Result: 0x%08X, bits: %d, res: %d \n", decodedResult.value, decodedResult.bits, res);  
    
         }
         else if (res == 2)
@@ -231,7 +280,7 @@ int main(void)
 
         }
 
-        k_sleep(K_MSEC(200));
+        k_sleep(K_MSEC(500));
         
     } 
     
